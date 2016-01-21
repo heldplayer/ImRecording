298982af2793d89b8759fc5cf6bfe7d192ce63da
Heldplayer <mitchel.pyl@gmail.com>
2016-01-21 21:06:31 +0100

    Update to 1.8.9

--- src/main/java/blue/heldplayer/mods/recording/client/TickHandler.java
+++ src/main/java/blue/heldplayer/mods/recording/client/TickHandler.java
@@ -5,7 +5,6 @@ import net.minecraft.client.gui.GuiMainMenu;
 import net.minecraft.client.gui.ScaledResolution;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.relauncher.Side;
@@ -16,7 +15,6 @@ import net.specialattack.forge.core.client.MC;
 public class TickHandler {
 
     public TickHandler() {
-        FMLCommonHandler.instance().bus().register(this);
         MinecraftForge.EVENT_BUS.register(this);
     }
 
@@ -31,7 +29,7 @@ public class TickHandler {
     public void onRenderTick(TickEvent.RenderTickEvent event) {
         if (event.phase == TickEvent.Phase.END) {
             Minecraft mc = MC.getMc();
-            ScaledResolution resolution = new ScaledResolution(mc, mc.displayWidth, mc.displayHeight);
+            ScaledResolution resolution = new ScaledResolution(mc);
 
             if (mc.currentScreen != null && mc.currentScreen.getClass() == GuiMainMenu.class) {
                 ClientProxy.overlay.drawScreen(mc, resolution, true);
@@ -43,7 +41,7 @@ public class TickHandler {
     public void renderGameOverlay(RenderGameOverlayEvent.Pre event) {
         if (event.type == RenderGameOverlayEvent.ElementType.PORTAL) {
             Minecraft mc = MC.getMc();
-            ScaledResolution resolution = new ScaledResolution(mc, mc.displayWidth, mc.displayHeight);
+            ScaledResolution resolution = new ScaledResolution(mc);
             ClientProxy.overlay.drawScreen(mc, resolution, false);
         }
     }
